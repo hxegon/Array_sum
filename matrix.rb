18b38612c5e0fbf66ef8556abed27bcc04093c02
Cooper Lebrun <aerowolf01@gmail.com>
2012-04-29 22:50:54 -0700

    first functional release

--- matrix.rb
+++ matrix.rb
@@ -4,13 +4,15 @@
 def input_to_hash_from_files(input, output)
     # takes input array of txt file nams and outputs them to a hash key line by line, and if a key repeats then its value += 1 !
     input.each do |list|
-        File.foreach(list, 'r') {|line| output[line] = #?#  }
+        IO.foreach(list) {|line| output[line.chomp] += 1}
     end
+    return output
 end
 
 def sort_hash_greatest_to_least(hash)
     # sort the hash keys from greatest to least value ?
     hash = hash.sort_by {|k, v| v}.reverse
+    return hash
 end
 
 def delete_0_value_pairs(hash)
@@ -26,8 +28,8 @@ def output_hash_keys_toFile(hash)
     # write the keys from a hash to a user named file line by line ?
     puts "enter output file name:"
     output_file = File.open(gets.chomp, 'w')
-    hash.each do |string| 
-        output_file.print(string[1])
+    hash.each do |k,v| 
+        output_file.puts(k.to_s)
     end
     output_file.close
     puts "Done!"
@@ -35,8 +37,8 @@ end
 
 output_hash = Hash.new(0)
 
-input_to_hash_from_files(Dir.glob('*.txt'), output_hash)
+output_hash = input_to_hash_from_files(Dir.glob('*.txt'), output_hash)
 print output_hash
-sort_hash_greatest_to_least(output_hash)
-delete_0_value_pairs(output_hash)
+output_hash = sort_hash_greatest_to_least(output_hash)
+output_hash.delete_if {|k,v| v <= 1}
 output_hash_keys_toFile(output_hash) 
