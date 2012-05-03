24233455b4a450bc14d39372ea1e9b2b7509ce24
Cooper Lebrun <aerowolf01@gmail.com>
2012-05-02 17:41:35 -0700

    refactoring code and updating documentation

--- matrix.rb
+++ matrix.rb
@@ -2,7 +2,7 @@
 # by Cooper LeBrun
 
 def input_to_hash_from_files(input, output)
-    # takes input array of txt file nams and outputs them to a hash key line by line, and if a key repeats then its value += 1 !
+    # takes input array of txt file names and outputs them to a hash key, line by line, and increases the value of the key by 1 
     input.each do |list|
         IO.foreach(list) {|line| output[line.chomp] += 1}
     end
@@ -10,13 +10,13 @@ def input_to_hash_from_files(input, output)
 end
 
 def sort_hash_greatest_to_least(hash)
-    # sort the hash keys from greatest to least value ?
+    # sort the hash keys from greatest to least val
     hash = hash.sort_by {|k, v| v}.reverse
     return hash
 end
 
 def delete_0_value_pairs(hash)
-    # sort out pairs with a 0 value ?
+    # sort out pairs with a 0 value
     hash.each do |key, value|
         if value == 0
             hash.delete!(key)
@@ -25,7 +25,7 @@ def delete_0_value_pairs(hash)
 end
 
 def output_hash_keys_toFile(hash)
-    # write the keys from a hash to a user named file line by line ?
+    # write the keys from a hash to a user named file line by line
     puts "enter output file name:"
     output_file = File.open(gets.chomp, 'w')
     hash.each do |k,v| 
@@ -35,10 +35,12 @@ def output_hash_keys_toFile(hash)
     puts "Done!"
 end
 
-output_hash = Hash.new(0)
+def matrix_gen
+    output_hash = Hash.new(0)
+    output_hash = input_to_hash_from_files(Dir.glob('*.txt'), output_hash)
+    output_hash = sort_hash_greatest_to_least(output_hash)
+    output_hash.delete_if {|k,v| v <= 1}
+    output_hash_keys_toFile(output_hash) 
+end
 
-output_hash = input_to_hash_from_files(Dir.glob('*.txt'), output_hash)
-print output_hash
-output_hash = sort_hash_greatest_to_least(output_hash)
-output_hash.delete_if {|k,v| v <= 1}
-output_hash_keys_toFile(output_hash) 
+matrix_gen
