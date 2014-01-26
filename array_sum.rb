73c83efb8d08206059c2dd2bb320f58ef2182a6d
Cooper LeBrun <cooperlebrun@gmail.com>
2014-01-25 19:50:53 -0800

    wow. this was much simpler as just a method tacked onto Array

--- array_sum.rb
+++ array_sum.rb
@@ -1,24 +1,13 @@
-class Array_Sum
+class Array
 
-  attr_reader :sum, :raw_items
-
-  def initialize *arrays
-    @raw_items = arrays.flatten
+  attr_reader :sum
+  
+  def sum
     @sum = Hash.new(0)
-    set_sum @raw_items
-  end
-
-  def << *arrays
-    array = arrays.flatten!
-    @raw_items << arrays
-    @raw_items.flatten!
-    set_sum array
-  end
-
-  def set_sum array
-    array.each do |item|
+    self.flatten.each do |item|
         @sum[item] += 1
     end
+    @sum
   end
 
 end
--- spec/array_sum_spec.rb
+++ spec/array_sum_spec.rb
@@ -1,74 +1,26 @@
 require 'spec_helper'
 
-describe Array_Sum do
+describe Array do
 
   before do
-    @a = Array_Sum.new
-    @attributes = %w[sum raw_items]
-    @items_a = %w[someitems are in this array someitems are in]
-    @items_b = %w[one two two three three three]
-  end
-
-  it 'has expected attributes' do
-    @attributes.each do |attr|
-      expect { eval "@a.#{attr}" }.to_not raise_error
-    end
-  end
-
-  it '#new takes a number of arrays or nothing' do
-    expect { Array_Sum.new }.not_to raise_error
-    expect { Array_Sum.new @items_a }.not_to raise_error
-    expect { Array_Sum.new @items_a, @items_a }.not_to raise_error
-  end
-
-  describe '#raw_items' do
-    it 'returns an empty array if no words were given' do
-      @a.raw_items.should_not be_nil
-      @a.raw_items.should be_kind_of Array
-      @a.raw_items.should be_empty
-    end
-
-    it 'returns a flat array if words is given' do
-      @a.raw_items.should be_kind_of Array
-      Array_Sum.new(@items_a).raw_items.should == @items_a
-      Array_Sum.new(@items_a, @items_a).raw_items.should == [@items_a, @items_a].flatten
-    end
-
-  end
-
-  describe '#<<' do
-
-    before do
-      @a = Array_Sum.new @items_a
-      @a << %w[more new words]
-    end
-
-    it 'sets #raw_items correctly' do
-      @a.raw_items.should == [@items_a, %w[more new words]].flatten
-    end
-
-    it 'sets #sum correctly' do
-      @a.sum.should == { 'someitems' => 2, 'are' => 2, 'in' => 2, 'this' => 1, 
-                         'array' => 1, 'more' => 1, 'new' => 1, 'words' => 1 }
-    end
+    @a = %w[someitems are in this array someitems are in]
+    @b = %w[one two two three three three]
   end
 
   describe '#sum' do
     it 'returns a hash with unique elements as keys and their counts as values' do
-      @a_a, @a_b = Array_Sum.new(@items_a), Array_Sum.new(@items_b)
-
-      [@a_a, @a_b].each do |a|
-        a.sum.should be_kind_of Hash
-        a.sum.should_not be_empty
+      [@a, @b].each do |array|
+        array.sum.should be_kind_of Hash
+        array.sum.should_not be_empty
       end
 
-      @a_a.sum.should == { 'someitems' => 2, 'are' => 2, 'in' => 2, 'this' => 1, 'array' => 1 }
-      @a_b.sum.should == { 'one' => 1, 'two' => 2, 'three' => 3}
+      @a.sum.should == { 'someitems' => 2, 'are' => 2, 'in' => 2, 'this' => 1, 'array' => 1 }
+      @b.sum.should == { 'one' => 1, 'two' => 2, 'three' => 3}
     end
 
     it "handles numbers properly" do
-      expect { @a_c = Array_Sum.new [1, 1, 3] }.not_to raise_error
-      @a_c.sum.should == { 1 => 2, 3 => 1 }
+      @c = [1, 1, 3]
+      @c.sum.should == { 1 => 2, 3 => 1 }
     end
   end
 end
